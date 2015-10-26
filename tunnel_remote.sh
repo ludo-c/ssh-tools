31d0036f217acec11a6d743e729d9a0369ac7ee1
Ludovic Chopin <lchopin+github@gmail.com>
2015-10-26 18:26:16 +0100

    tunnel_remote.sh: do not run if already running

--- tunnel_remote.sh
+++ tunnel_remote.sh
@@ -4,6 +4,21 @@
 # http://artisan.karma-lab.net/faire-passer-trafic-tunnel-ssh
 
 options="ExitOnForwardFailure yes"
+name=$(basename $0)
+lf=/tmp/${name}_pid_lock_file
+
+# http://stackoverflow.com/questions/1440967/how-do-i-make-sure-my-bash-script-isnt-already-running
+# create empty lock file if none exists
+touch $lf
+read lastPID < $lf
+# if lastPID is not null and a process with that pid exists, exit
+if [ ! -z "$lastPID" -a -d /proc/$lastPID ]; then
+	echo "${name} already running"
+	exit 1
+fi
+
+# save my pid in the lock file
+echo $$ > $lf
 
 while [ true ]; do
 	date
