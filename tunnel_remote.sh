f2272aec37d1abf57afd4cadc4869e8b25c146f5
Ludovic Chopin <lchopin+github@gmail.com>
2015-10-29 19:01:17 +0100

    tunnel_remote.sh: cosmetic

--- tunnel_remote.sh
+++ tunnel_remote.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
-
+# Create a ssh remote port forwarding
 # http://artisan.karma-lab.net/faire-passer-trafic-tunnel-ssh
+# Returns : 0 ok
+#           1 process is already running
+#           2 bad parameters
 
-options="ExitOnForwardFailure yes"
 name=$(basename $0)
-lf=/tmp/${name}_pid_lock_file
+lf=/tmp/${name}.pid
+options="ExitOnForwardFailure=yes"
 
 if [ "$#" -ne 3 ]; then
 	echo "Bad parameters"
-	echo "use : ./tunnel_remote.sh <remote_port> <local_port> <dest_host>"
-	echo "eg : ./tunnel_remote.sh 2222 22 bob@host"
+	echo "Usage : ${name} <remote_port> <local_port> <dest_host>"
+	echo "eg : ${name} 2222 22 bob@host"
 	exit 2
 fi
 
@@ -19,19 +22,19 @@ dest_host=$3
 
 # http://stackoverflow.com/questions/1440967/how-do-i-make-sure-my-bash-script-isnt-already-running
 # create empty lock file if none exists
-touch $lf
-read lastPID < $lf
+touch ${lf}
+read lastPID < ${lf}
 # if lastPID is not null and a process with that pid exists, exit
-if [ ! -z "$lastPID" -a -d /proc/$lastPID ]; then
+if [ ! -z "${lastPID}" -a -d /proc/${lastPID} ]; then
 	echo "${name} already running"
 	exit 1
 fi
 
 # save my pid in the lock file
-echo $$ > $lf
+echo $$ > ${lf}
 
 while [ true ]; do
 	date
-	ssh -o "${options}" -nTNR ${remote_port}:localhost:${local_port} ${dest_host}
+	ssh -o ${options} -nTNR ${remote_port}:localhost:${local_port} ${dest_host}
 	sleep 10
 done
