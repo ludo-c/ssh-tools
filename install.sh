64b6eca5f014ea1b304a36e98a63ffecc7299178
Ludovic Chopin <lchopin+github@gmail.com>
2015-11-27 12:43:55 +0100

    install.sh: install systemd files

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 name=$(basename $0)
 bin_dir="${HOME}/bin"
+systemd_conf_dir="/etc/systemd/system"
 
 [ ! -d "$bin_dir" ] && mkdir ${bin_dir}
 
@@ -17,3 +18,19 @@ for script in *; do
 	fi
 done
 
+type systemctl > /dev/null 2> /dev/null
+if [ $? -eq 0 ]; then
+	for script in *.service; do
+		if [ ! -f ${systemd_conf_dir}/${script} ];then
+			ln -s ${PWD}/${script} ${systemd_conf_dir}
+			if [ $? -eq 0 ]; then
+				echo "link created for ${script}"
+				echo "enable it with : systemctl enable" ${script}
+			else
+				echo "Error while creating link to ${systemd_conf_dir}. Need to be root"
+			fi
+		fi
+	done
+else
+	echo "Systemd isn't available on this system. Startup files will not be installed"
+fi
