417d4faebc8570edf8c053883c4a9bf6077465e1
Ludovic Chopin <lchopin+github@gmail.com>
2015-12-05 21:42:14 +0100

    systemd files: use systemd --user

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 name=$(basename $0)
 bin_dir="${HOME}/bin"
-systemd_conf_dir="/etc/systemd/system"
+systemd_conf_dir="${HOME}/.config/systemd/user"
 
-[ ! -d "$bin_dir" ] && mkdir ${bin_dir}
+[ ! -d "${bin_dir}" ] && mkdir ${bin_dir}
 
 for script in *; do
 	# link only files that are not already linked
@@ -20,18 +20,16 @@ done
 
 type systemctl > /dev/null 2> /dev/null
 if [ $? -eq 0 ]; then
+	[ ! -d "${systemd_conf_dir}" ] && mkdir ${systemd_conf_dir}
 	for script in *.service; do
 		if [ ! -f ${systemd_conf_dir}/${script} ];then
 			# in order to "enable" service the file have to be copied, not symlinked
 			cp ${PWD}/${script} ${systemd_conf_dir}
-			if [ $? -eq 0 ]; then
-				echo "${script} copied"
-				echo "enable it with :"
-				echo "sudo systemctl daemon-reload"
-				echo "sudo systemctl enable" ${script}
-			else
-				echo "Error while copying to ${systemd_conf_dir}. Need to be root"
-			fi
+			echo "${script} copied"
+			echo "enable it with :"
+			echo "systemctl --user daemon-reload"
+			echo "systemctl --user enable" ${script}
+			echo "sudo loginctl enable-linger ${USER}"
 		fi
 	done
 else
