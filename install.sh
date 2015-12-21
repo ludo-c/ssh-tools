b5e9e34da13b6c43e6184f29b6526edec0ec5098
Ludovic Chopin <lchopin+github@gmail.com>
2015-12-21 18:25:32 +0100

    install.sh: can create several links

--- install.sh
+++ install.sh
@@ -1,38 +1,31 @@
 #!/bin/sh
+# usage :
+# install_script.sh [links names]
+# Can be used without arguments, create only tunnel.sh
+# with one or more arguments, create links with thoses names
 
 name=$(basename $0)
 bin_dir="${HOME}/bin"
 systemd_conf_dir="${HOME}/.config/systemd/user"
+origin="tunnel.sh"
 
 [ ! -d "${bin_dir}" ] && mkdir -p ${bin_dir}
 
-for script in *; do
-	# link only files that are not already linked
-	# only executable files
-	# do not create link for this script
-	if [ ! -f ${bin_dir}/${script} ] && \
-	   [ -x ${script} ] && \
-	   [ "${script}" != "${name}" ]; then
-		ln -s ${PWD}/${script} ${bin_dir}
-		echo "link created for ${script}"
+if [ $# -eq 0 ]; then
+	if [ ! -f ${bin_dir}/${origin} ]; then
+		ln -s ${PWD}/${origin} ${bin_dir}/
+		${bin_dir}/${origin} create config file
+	else
+		echo "${bin_dir}/${origin}: file exists"
 	fi
-done
-echo "To create another links: ln -s ${PWD}/${script} ${bin_dir}/foo.sh"
-
-type systemctl > /dev/null 2> /dev/null
-if [ $? -eq 0 ]; then
-	[ ! -d "${systemd_conf_dir}" ] && mkdir -p ${systemd_conf_dir}
-	for script in *.service; do
-		if [ ! -f ${systemd_conf_dir}/${script} ];then
-			# in order to "enable" service the file have to be copied, not symlinked
-			cp ${PWD}/${script} ${systemd_conf_dir}
-			echo "${script} copied"
-			echo "enable it with :"
-			echo "systemctl --user daemon-reload"
-			echo "systemctl --user enable" ${script}
-			echo "sudo loginctl enable-linger ${USER}"
+else
+	for script in $*
+	do
+		# link only files that are not already linked
+		if [ ! -f ${bin_dir}/${script} ]; then
+			ln -s ${PWD}/${origin} ${bin_dir}/${script}
+			echo "link created for ${script}"
+			${bin_dir}/${script} create config file
 		fi
 	done
-else
-	echo "Systemd isn't available on this system. Startup files will not be installed"
 fi
